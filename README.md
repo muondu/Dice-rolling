0ac2724e6565e5af4a4db83743aa88faa51705f2
muondu <munenemuondu@gmail.com>
2020-04-23 09:56:04 +0300

    I have made some changes

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dice rolling
+This project are two dices. The computer will chose two random numbers between  1-6.
--- roling.py
+++ roling.py
@@ -2,7 +2,9 @@ import random
 print("Today, we are going to play role the dice.")
 a = 1
 b = 6
-c = print(random.randrange(a, b))
+fistDice = print(random.randrange(a, b))
+
+secondDice = print(random.randrange(a, b))
 d = input("Do you want to try again?  ")
 
 def whileloop():
