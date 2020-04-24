9bca5dbcced7283833cb70b8e6f87cd66be6a7f1
muondu <munenemuondu@gmail.com>
2020-04-24 11:51:03 +0300

    I have made some changes

--- roling.py
+++ roling.py
@@ -3,6 +3,7 @@ import sys
 
 def mycode():
 	print("Today, we are going to play role the dice.")
+	print("This game gives you random numbers. The first random number is the first dice and the second number is the second dice")
 
 	fistDice = print(random.randrange(1, 6))
 	secondDice = print(random.randrange(1, 6))
